a45267bfa0fb6ce8c1524021e1565c2f46e04299
asaf <49835614+lacherogwu@users.noreply.github.com>
2025-03-01 19:04:57 +0100

    fix: support args

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
 const cache = new WeakMap();
-export function cacheFunc<T>(fn: (...args: any[]) => T): () => T {
-	return () => {
+export function cacheFunc<T extends (...args: any[]) => any>(fn: T): T {
+	return ((...args: Parameters<T>): ReturnType<T> => {
 		if (!cache.has(fn)) {
-			cache.set(fn, fn());
+			cache.set(fn, fn(...args));
 		}
 		return cache.get(fn);
-	};
+	}) as T;
 }
